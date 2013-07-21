124496f78efc15ba80abafb9392a8b2bbce2006d
Kenji Urushima <kenji.urushima@gmail.com>
2013-07-21 09:14:11 +0900

    base64x.js is moved from jsjws

---
+++ base64x-1.1.js
@@ -0,0 +1,349 @@
+/*! base64x-1.1.2 (c) 2013 Kenji Urushima | kjur.github.com/jsjws/license
+ */
+/*
+ * base64x.js - Base64url and supplementary functions for Tom Wu's base64.js library
+ *
+ * version: 1.1.2 (2013 Jul 21)
+ *
+ * Copyright (c) 2012-2013 Kenji Urushima (kenji.urushima@gmail.com)
+ *
+ * This software is licensed under the terms of the MIT License.
+ * http://kjur.github.com/jsjws/license/
+ *
+ * The above copyright and license notice shall be 
+ * included in all copies or substantial portions of the Software.
+ *
+ * DEPENDS ON:
+ *   - base64.js - Tom Wu's Base64 library
+ */
+
+/**
+ * Base64URL and supplementary functions for Tom Wu's base64.js library.<br/>
+ * This class is just provide information about global functions
+ * defined in 'base64x.js'. The 'base64x.js' script file provides
+ * global functions for converting following data each other.
+ * <ul>
+ * <li>(ASCII) String</li>
+ * <li>UTF8 String including CJK, Latin and other characters</li>
+ * <li>byte array</li>
+ * <li>hexadecimal encoded String</li>
+ * <li>Full URIComponent encoded String (such like "%69%94")</li>
+ * <li>Base64 encoded String</li>
+ * <li>Base64URL encoded String</li>
+ * </ul>
+ * All functions in 'base64x.js' are defined in {@link _global_} and not
+ * in this class.
+ * 
+ * @class Base64URL and supplementary functions for Tom Wu's base64.js library
+ * @author Kenji Urushima
+ * @version 1.1 (07 May 2012)
+ * @requires base64.js
+ * @see <a href="http://kjur.github.com/jsjws/">'jwjws'(JWS JavaScript Library) home page http://kjur.github.com/jsjws/</a>
+ * @see <a href="http://kjur.github.com/jsrsasigns/">'jwrsasign'(RSA Sign JavaScript Library) home page http://kjur.github.com/jsrsasign/</a>
+ */
+function Base64x() {
+}
+
+// ==== string / byte array ================================
+/**
+ * convert a string to an array of character codes
+ * @param {String} s
+ * @return {Array of Numbers} 
+ */
+function stoBA(s) {
+    var a = new Array();
+    for (var i = 0; i < s.length; i++) {
+	a[i] = s.charCodeAt(i);
+    }
+    return a;
+}
+
+/**
+ * convert an array of character codes to a string
+ * @param {Array of Numbers} a array of character codes
+ * @return {String} s
+ */
+function BAtos(a) {
+    var s = "";
+    for (var i = 0; i < a.length; i++) {
+	s = s + String.fromCharCode(a[i]);
+    }
+    return s;
+}
+
+// ==== byte array / hex ================================
+/**
+ * convert an array of bytes(Number) to hexadecimal string.<br/>
+ * @param {Array of Numbers} a array of bytes
+ * @return {String} hexadecimal string
+ */
+function BAtohex(a) {
+    var s = "";
+    for (var i = 0; i < a.length; i++) {
+	var hex1 = a[i].toString(16);
+	if (hex1.length == 1) hex1 = "0" + hex1;
+	s = s + hex1;
+    }
+    return s;
+}
+
+// ==== string / hex ================================
+/**
+ * convert a ASCII string to a hexadecimal string of ASCII codes.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} hexadecimal string
+ */
+function stohex(s) {
+    return BAtohex(stoBA(s));
+}
+
+// ==== string / base64 ================================
+/**
+ * convert a ASCII string to a Base64 encoded string.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} Base64 encoded string
+ */
+function stob64(s) {
+    return hex2b64(stohex(s));
+}
+
+// ==== string / base64url ================================
+/**
+ * convert a ASCII string to a Base64URL encoded string.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} Base64URL encoded string
+ */
+function stob64u(s) {
+    return b64tob64u(hex2b64(stohex(s)));
+}
+
+/**
+ * convert a Base64URL encoded string to a ASCII string.<br/>
+ * NOTE: This can't be used for Base64URL encoded non ASCII characters.
+ * @param {s} s Base64URL encoded string
+ * @return {String} ASCII string
+ */
+function b64utos(s) {
+    return BAtos(b64toBA(b64utob64(s)));
+}
+
+// ==== base64 / base64url ================================
+/**
+ * convert a Base64 encoded string to a Base64URL encoded string.<br/>
+ * Example: "ab+c3f/==" &rarr; "ab-c3f_"
+ * @param {String} s Base64 encoded string
+ * @return {String} Base64URL encoded string
+ */
+function b64tob64u(s) {
+    s = s.replace(/\=/g, "");
+    s = s.replace(/\+/g, "-");
+    s = s.replace(/\//g, "_");
+    return s;
+}
+
+/**
+ * convert a Base64URL encoded string to a Base64 encoded string.<br/>
+ * Example: "ab-c3f_" &rarr; "ab+c3f/=="
+ * @param {String} s Base64URL encoded string
+ * @return {String} Base64 encoded string
+ */
+function b64utob64(s) {
+    if (s.length % 4 == 2) s = s + "==";
+    else if (s.length % 4 == 3) s = s + "=";
+    s = s.replace(/-/g, "+");
+    s = s.replace(/_/g, "/");
+    return s;
+}
+
+// ==== hex / base64url ================================
+/**
+ * convert a hexadecimal string to a Base64URL encoded string.<br/>
+ * @param {String} s hexadecimal string
+ * @return {String} Base64URL encoded string
+ */
+function hextob64u(s) {
+    return b64tob64u(hex2b64(s));
+}
+
+/**
+ * convert a Base64URL encoded string to a hexadecimal string.<br/>
+ * @param {String} s Base64URL encoded string
+ * @return {String} hexadecimal string
+ */
+function b64utohex(s) {
+    return b64tohex(b64utob64(s));
+}
+
+var utf8tob64u, b64utoutf8;
+
+if (typeof Buffer === 'function')
+{
+  utf8tob64u = function (s)
+  {
+    return b64tob64u(new Buffer(s, 'utf8').toString('base64'));
+  };
+
+  b64utoutf8 = function (s)
+  {
+    return new Buffer(b64utob64(s), 'base64').toString('utf8');
+  };
+}
+else
+{
+// ==== utf8 / base64url ================================
+/**
+ * convert a UTF-8 encoded string including CJK or Latin to a Base64URL encoded string.<br/>
+ * @param {String} s UTF-8 encoded string
+ * @return {String} Base64URL encoded string
+ * @since 1.1
+ */
+  utf8tob64u = function (s)
+  {
+    return hextob64u(uricmptohex(encodeURIComponentAll(s)));
+  };
+
+/**
+ * convert a Base64URL encoded string to a UTF-8 encoded string including CJK or Latin.<br/>
+ * @param {String} s Base64URL encoded string
+ * @return {String} UTF-8 encoded string
+ * @since 1.1
+ */
+  b64utoutf8 = function (s)
+  {
+    return decodeURIComponent(hextouricmp(b64utohex(s)));
+  };
+}
+
+// ==== utf8 / base64url ================================
+/**
+ * convert a UTF-8 encoded string including CJK or Latin to a Base64 encoded string.<br/>
+ * @param {String} s UTF-8 encoded string
+ * @return {String} Base64 encoded string
+ * @since 1.1.1
+ */
+function utf8tob64(s) {
+  return hex2b64(uricmptohex(encodeURIComponentAll(s)));
+}
+
+/**
+ * convert a Base64 encoded string to a UTF-8 encoded string including CJK or Latin.<br/>
+ * @param {String} s Base64 encoded string
+ * @return {String} UTF-8 encoded string
+ * @since 1.1.1
+ */
+function b64toutf8(s) {
+  return decodeURIComponent(hextouricmp(b64tohex(s)));
+}
+
+// ==== utf8 / hex ================================
+/**
+ * convert a UTF-8 encoded string including CJK or Latin to a hexadecimal encoded string.<br/>
+ * @param {String} s UTF-8 encoded string
+ * @return {String} hexadecimal encoded string
+ * @since 1.1.1
+ */
+function utf8tohex(s) {
+  return uricmptohex(encodeURIComponentAll(s));
+}
+
+/**
+ * convert a hexadecimal encoded string to a UTF-8 encoded string including CJK or Latin.<br/>
+ * Note that when input is improper hexadecimal string as UTF-8 string, this function returns
+ * 'null'.
+ * @param {String} s hexadecimal encoded string
+ * @return {String} UTF-8 encoded string or null
+ * @since 1.1.1
+ */
+function hextoutf8(s) {
+  return decodeURIComponent(hextouricmp(s));
+}
+
+/**
+ * convert a hexadecimal encoded string to raw string including non printable characters.<br/>
+ * @param {String} s hexadecimal encoded string
+ * @return {String} raw string
+ * @since 1.1.2
+ * @example
+ * hextorstr("610061") &rarr; "a\x00a"
+ */
+function hextorstr(sHex) {
+    var s = "";
+    for (var i = 0; i < sHex.length - 1; i += 2) {
+	s += String.fromCharCode(parseInt(sHex.substr(i, 2), 16));
+    }
+    return s;
+}
+
+// ==== URIComponent / hex ================================
+/**
+ * convert a URLComponent string such like "%67%68" to a hexadecimal string.<br/>
+ * @param {String} s URIComponent string such like "%67%68"
+ * @return {String} hexadecimal string
+ * @since 1.1
+ */
+function uricmptohex(s) {
+  return s.replace(/%/g, "");
+}
+
+/**
+ * convert a hexadecimal string to a URLComponent string such like "%67%68".<br/>
+ * @param {String} s hexadecimal string
+ * @return {String} URIComponent string such like "%67%68"
+ * @since 1.1
+ */
+function hextouricmp(s) {
+  return s.replace(/(..)/g, "%$1");
+}
+
+// ==== URIComponent ================================
+/**
+ * convert UTFa hexadecimal string to a URLComponent string such like "%67%68".<br/>
+ * Note that these "<code>0-9A-Za-z!'()*-._~</code>" characters will not
+ * converted to "%xx" format by builtin 'encodeURIComponent()' function.
+ * However this 'encodeURIComponentAll()' function will convert 
+ * all of characters into "%xx" format.
+ * @param {String} s hexadecimal string
+ * @return {String} URIComponent string such like "%67%68"
+ * @since 1.1
+ */
+function encodeURIComponentAll(u8) {
+  var s = encodeURIComponent(u8);
+  var s2 = "";
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == "%") {
+      s2 = s2 + s.substr(i, 3);
+      i = i + 2;
+    } else {
+      s2 = s2 + "%" + stohex(s[i]);
+    }
+  }
+  return s2;
+}
+
+// ==== new lines ================================
+/**
+ * convert all DOS new line("\r\n") to UNIX new line("\n") in 
+ * a String "s".
+ * @param {String} s string 
+ * @return {String} converted string
+ */
+function newline_toUnix(s) {
+    s = s.replace(/\r\n/mg, "\n");
+    return s;
+}
+
+/**
+ * convert all UNIX new line("\r\n") to DOS new line("\n") in 
+ * a String "s".
+ * @param {String} s string 
+ * @return {String} converted string
+ */
+function newline_toDos(s) {
+    s = s.replace(/\r\n/mg, "\n");
+    s = s.replace(/\n/mg, "\r\n");
+    return s;
+}
+
