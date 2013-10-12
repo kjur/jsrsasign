e6cf70425a99b770ee1e6b5686fbff2f1c79f45d
Kenji Urushima <kenji.urushima@gmail.com>
2013-10-12 17:32:41 +0900

    npm 0.0.3 release

--- npm/lib/footer.js
+++ npm/lib/footer.js
@@ -9,6 +9,34 @@ exports.Mac = KJUR.crypto.Mac;
 exports.KEYUTIL = KEYUTIL;
 exports.ASN1HEX = ASN1HEX;
 
+// ext/base64.js
+exports.b64tohex = b64tohex;
+exports.b64tohex = b64toBA;
+
+// base64x.js
+exports.stoBA = stoBA;
+exports.stoBA = BAtos;
+exports.stoBA = BAtohex;
+exports.stohex = stohex;
+exports.stob64 = stob64;
+exports.stob64u = stob64u;
+exports.b64utos = b64utos;
+exports.b64tob64u = b64tob64u;
+exports.b64utob64 = b64utob64;
+exports.hextob64u = hextob64u;
+exports.b64utohex = b64utohex;
+exports.b64tohex = b64tohex;
+exports.utf8tob64u = utf8tob64u;
+exports.b64utoutf8 = b64utoutf8;
+exports.utf8tob64 = utf8tob64;
+exports.b64toutf8 = b64toutf8;
+exports.utf8tohex = utf8tohex;
+exports.hextoutf8 = hextoutf8;
+exports.hextorstr = hextorstr;
+exports.rstrtohex = rstrtohex;
+exports.newline_toUnix = newline_toUnix;
+exports.newline_toDos = newline_toDos;
+
 exports.crypto = KJUR.crypto;
 exports.asn1 = KJUR.asn1;
 exports.jws = KJUR.jws;
