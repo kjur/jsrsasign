92f12393350930bc2468f4ebd06d86323ef373a6
Kenji Urushima <kenji.urushima@gmail.com>
2015-08-30 13:56:17 +0900

    4.8.5 release

--- jws-3.2.js
+++ jws-3.2.js
@@ -1,9 +1,9 @@
-/*! jws-3.2.3 (c) 2013-2015 Kenji Urushima | kjur.github.com/jsrsasign/license
+/*! jws-3.2.4 (c) 2013-2015 Kenji Urushima | kjur.github.com/jsrsasign/license
  */
 /*
  * jws.js - JSON Web Signature Class
  *
- * version: 3.2.3 (2015 May 29)
+ * version: 3.2.4 (2015 Aug 30)
  *
  * Copyright (c) 2010-2015 Kenji Urushima (kenji.urushima@gmail.com)
  *
@@ -18,7 +18,7 @@
  * @fileOverview
  * @name jws-3.2.js
  * @author Kenji Urushima kenji.urushima@gmail.com
- * @version 3.2.3 (2015-May-29)
+ * @version 3.2.4 (2015-Aug-30)
  * @since jsjws 1.0, jsrsasign 4.8.0
  * @license <a href="http://kjur.github.io/jsrsasign/license/">MIT License</a>
  */
@@ -669,7 +669,7 @@ KJUR.jws.JWS.verify = function(sJWS, key, acceptAlgs) {
  * <li>Payload.iat (issued at) - Validation time is greater than Payloead.iat.</li>
  * </ul>
  * </li>
- * <li>Payload.jti (JWT id) - Payload.jti is defined.</li>
+ * <li>Payload.jti (JWT id) - Payload.jti is included in acceptField.jti if specified. (OPTION)</li>
  * <li>JWS signature of JWS is valid for specified key.</li>
  * </ul>
  *
@@ -685,6 +685,7 @@ KJUR.jws.JWS.verify = function(sJWS, key, acceptAlgs) {
  *   sub: ['mailto:john@foo.com', 'mailto:alice@foo.com'],
  *   verifyAt: KJUR.jws.IntDate.get('20150520235959Z'),
  *   aud: ['http://foo.com'], // aud: 'http://foo.com' is fine too.
+ *   jti: 'id123456'
  * });
  */
 KJUR.jws.JWS.verifyJWT = function(sJWT, key, acceptField) {
@@ -752,7 +753,9 @@ KJUR.jws.JWS.verifyJWT = function(sJWT, key, acceptField) {
     }
 
     // 9 JWT id 'jti' check
-    if (pPayload.jti === undefined) return false;
+    if (pPayload.jti !== undefined && acceptField.jti !== undefined) {
+      if (pPayload.jti !== acceptField.jti) return false;
+    }
 
     // 10 JWS signature check
     if (! KJUR.jws.JWS.verify(sJWT, key, acceptField.alg)) return false;
