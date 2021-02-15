dbd30e0a390227734284facda41ce7ffddea1cc4
Kenji Urushima <kenji.urushima@gmail.com>
2021-02-15 11:22:59 +0900

    jsrsasign-util update

--- npm_util/lib/jsrsasign-util.js
+++ npm_util/lib/jsrsasign-util.js
@@ -5,6 +5,7 @@ exports.readFileUTF8 = readFileUTF8;
 exports.readFileHexByBin = readFileHexByBin;
 exports.readFile = readFile;
 exports.saveFile = saveFile;
+exports.saveFileUTF8 = saveFileUTF8;
 exports.saveFileBinByHex = saveFileBinByHex;
 exports.readJSON = readJSON;
 exports.readJSONC = readJSONC;
