97921fb2a0a0e1acafadf1a8247e3d6ace1ceeaa
Kenji Urushima <kenji.urushima@gmail.com>
2023-04-15 12:12:14 +0900

    10.8.2 release

--- jsrsasign-jwths-min.js
+++ jsrsasign-jwths-min.js
@@ -1,8 +1,8 @@
 /*
- * jsrsasign(jwths) 10.8.1 (2023-04-09) (c) 2010-2023 Kenji Urushima | kjur.github.io/jsrsasign/license
+ * jsrsasign(jwths) 10.8.2 (2023-04-15) (c) 2010-2023 Kenji Urushima | kjur.github.io/jsrsasign/license
  */
-var VERSION = "10.8.1";
-var VERSION_FULL = "jsrsasign(jwths) 10.8.1 (2023-04-09) (c) 2010-2023 Kenji Urushima | kjur.github.io/jsrsasign/license";
+var VERSION = "10.8.2";
+var VERSION_FULL = "jsrsasign(jwths) 10.8.2 (2023-04-15) (c) 2010-2023 Kenji Urushima | kjur.github.io/jsrsasign/license";
 
 /*! CryptoJS v3.1.2 core-fix.js
  * code.google.com/p/crypto-js
